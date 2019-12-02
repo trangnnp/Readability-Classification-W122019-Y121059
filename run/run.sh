458374a2bfe04e51cbd13f68740b22dcff114345
TrangNNP <trangnnp@TrangNNPs-MacBook-Pro.local>
2019-12-02 20:16:13 +0700

    Add Dataset. Setup basics. First commit

---
+++ run/run.sh
@@ -0,0 +1,9 @@
+# /**
+# * @ Author: Trang Ngoc-Phuong Nguyen
+# * @ Create Time: 2019-12-02 16:59:11
+# * @ Modified by: Trang Ngoc-Phuong Nguyen
+# * @ Modified time: 2019-12-02 20:12:16
+# * @ Description:
+# */
+
+python3 sourcecode/main.py
---
+++ sourcecode/main.py
@@ -0,0 +1,12 @@
+'''
+ # @ Author: Trang Ngoc-Phuong Nguyen
+ # @ Create Time: 2019-12-02 16:31:56
+ # @ Modified by: Trang Ngoc-Phuong Nguyen
+ # @ Modified time: 2019-12-02 20:12:02
+ # @ Description:
+ '''
+
+import datetime
+
+print(datetime.datetime.now())
+print("Hello world!")
