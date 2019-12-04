3d0e3d345e0a87c2a65e1f554ab5dea2da12334e
TrangNNP <trangnnp@TrangNNPs-MacBook-Pro.local>
2019-12-04 13:18:24 +0700

    Add features

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -12,19 +12,23 @@ import datetime
 from config import config as c
 from log import log as l
 from helper import help as h
+from process import process as p
 
 
 def main():
+    l.startProj()
     l.callHere('Hello world!')
     print("Hello world!")
 
     sys.path.append(os.path.join(sys.path[0], 'config'))
     sys.path.append(os.path.join(sys.path[0], 'helper'))
     sys.path.append(os.path.join(sys.path[0], 'log'))
+    sys.path.append(os.path.join(sys.path[0], 'process'))
 
-    # c.loadConfig()
+    p.main()
 
     l.exitHere('Exit All!')
+    print("Done!")
 
 
 if __name__ == "__main__":
