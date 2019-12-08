70a5ab3a1141efa06ebb7ca374ad9f3d2ac007dd
TrangNNP <trangnnp@TrangNNPs-MacBook-Pro.local>
2019-12-08 14:11:42 +0700

    new scoring system with coe

--- sourcecode/helper/help.py
+++ sourcecode/helper/help.py
@@ -13,7 +13,22 @@ from log import log as l
 import statistics
 import re
 import random
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from collections import Counter
+import operator
+from random import randrange
+from helper import help as h
+from config import constant as cc
+from config import config as c
 import numpy
+import string
+import re
+import os
+import sys
+sys.setrecursionlimit(1000000)
 
 
 def syllableCount(word):
@@ -143,6 +158,240 @@ def syllableCount(word):
 
     # calculate the output
     return numVowels - disc + syls
+
+
+# fi = open('coe.txt', 'w')
+
+def setupCoefficient(level, featureCount, unit, topLimit, bottomLimit):
+    l.startHere('Start setup Coefficient')
+    coe = {}
+    fold = int(cc.FOLD)
+
+    curSubSet = []
+    for i in range(featureCount):
+        curSubSet.append(0.0)
+
+    newCoeSet(-1, featureCount,
+              curSubSet, unit, topLimit, bottomLimit)
+    print("Coe Done")
+    fi.close()
+
+    return coe
+
+
+def newCoeSet(num, featureCount, curSubSet, unit, topLimit, bottomLimit):
+    if sum(curSubSet) > 1:
+        return
+    num += 1
+    if num == featureCount-1:
+        curSubSet[-1] = 1-sum(curSubSet[:-1])
+        fi.write(str(curSubSet)+'\n')
+        curSubSet[-1] = 0.0
+        return
+
+    for i in numpy.arange(bottomLimit-unit, topLimit+unit, unit):
+        curSubSet[num] = i
+        newCoeSet(num, featureCount, curSubSet,
+                  unit, topLimit, bottomLimit)
+        curSubSet[num] = 0.0
+
+    return
+
+
+def tokenize(document):
+    document = document.replace('-', '')
+    document = document.replace('\n', ' ')
+    tokenizer = RegexpTokenizer(r'[A-Za-z0-9]+')
+    # TODO: remove white spaces
+    # TODO: tokenize
+    document = tokenizer.tokenize(document)
+    return document
+
+
+def preprocess(document):
+    processed_word = []
+    # TODO: lowercase
+    document = document.lower()
+    # TODO: remove number
+    document = ''.join([i for i in document if (not i.isdigit())])
+    # TODO: remove punctuation
+    document = document.replace('-', '')
+    tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+    # TODO: remove white spaces
+    # TODO: tokenize
+    processed_word = tokenizer.tokenize(document)
+    # TODO: remove stopword
+    processed_word = [
+        word for word in processed_word if word not in stopwords.words('english')]
+    # stemming/lemmatization
+    lemmatizer = WordNetLemmatizer()
+    processed_word = [lemmatizer.lemmatize(word) for word in processed_word]
+    # processed_word = [stemmer.stem(word) for word in processed_word]
+    return processed_word
+
+
+def setupFold(quantyLevel):
+    l.startHere('Start setup FOLD')
+    foldDict = []
+    fold = int(cc.FOLD)
+
+    for i in range(fold):
+        foldDict.append({})
+        for j in range(len(quantyLevel)):
+            foldDict[-1][j] = []
+            for id in range(int(quantyLevel[j]/fold)*i+1, int(quantyLevel[j]/fold)*(i+1)+1):
+                foldDict[-1][j].append(id)
+    l.startHere('Setup FOLD Done')
+    return foldDict
+
+
+def calculateRawScore(level, quantyLevel, featureCount):
+    print("CalculateRawScore start")
+    levelDocs = {}
+    preprocessLevelDocs = {}
+    scoreDoc = {}
+    for i in range(len(level)):
+        levelDocs[i], preprocessLevelDocs[i] = readLevel(
+            level[i], quantyLevel[i])
+    print("Read raw data done...")
+
+    raw = {}
+    for i in range(len(level)):
+        raw[i] = []
+        maxx = []
+        for _ in range(featureCount):
+            raw[i].append([])
+            maxx.append(-1)
+
+        for id in range(quantyLevel[i]):
+            rawScoreID = h.getRawScore(
+                levelDocs[i][id], preprocessLevelDocs[i][id])
+            for index in range(featureCount):
+                raw[i][index].append(rawScoreID[index])
+        print(raw[i])
+
+        for index in range(featureCount):
+            maxx[index] = max(max(raw[i][index]), maxx[index])
+
+        # print(raw)
+    # print(maxx)
+
+    for i in range(len(level)):
+        for id in range(quantyLevel[i]):
+            for index in range(featureCount):
+                raw[i][index][id] = float(raw[i][index][id])/float(maxx[index])
+
+        fo = open('./dataset/corpus/'+level[i]+'.txt', 'w')
+
+        for id in range(quantyLevel[i]):
+            for index in range(featureCount):
+                fo.write(str(raw[i][index][id]) + ' ')
+            fo.write('\n')
+
+
+def getRawScoreFromFile(level, quantyLevel):
+    raw = {}
+
+    for i in range(len(level)):
+        f = open('./dataset/corpus/'+level[i]+'.txt', 'r')
+        raw[i] = []
+        rl = f.readlines()
+        for x in rl:
+            x = x[:-2]
+            arr = list(map(float, x.split(' ')))
+            raw[i].append(arr)
+    print("Read Raw Score from Files Done")
+    return raw
+
+
+def getCoedScore(coef, rawScore):
+    score = []
+    for le in range(len(rawScore)):
+        score.append([])
+        for i in range(len(rawScore[le])):
+            element = []
+            for co in range(len(coef)):
+                element.append(rawScore[le][i][co]*coef[co])
+            element.append(sum(element))
+            score[le].append(element)
+    return score
+
+
+def getCoe(fileName):
+    f = open(fileName, 'r')
+    coe = []
+    rl = f.readlines()
+    for x in rl:
+        x = x[:-2]
+        x = x[1:]
+        arr = list(map(float, x.split(', ')))
+        coe.append(arr)
+    return coe
+    # print(coe)
+
+
+def getRawScore(raw, preprocessed):
+    score = {
+        "lexical_density":      0.0,
+        "char_per_word":        0.0,
+        "type_token_ratio":     0.0,
+        "correct_type_token_ratio":     0.0,
+        "syllable_count":       0.0,
+        "document_length":      0.0,
+        "awl":                  0.0
+    }
+
+    voca = set(preprocessed)
+    vocaRaw = set(raw)
+    type_token_ratio = "type_token_ratio"
+    correct_type_token_ratio = "correct_type_token_ratio"
+    lexical_density = "lexical_density"
+    char_per_word = "char_per_word"
+    syllable_count = "syllable_count"
+    document_length = "document_length"
+    awl = "awl"
+
+    # Token related
+    # ___type_token_ratio
+    score[type_token_ratio] = float(len(voca))/float(len(raw))
+    score[correct_type_token_ratio] = float(len(voca))/float((2*len(raw))**0.5)
+    # l.resultHere('type_token_ratio: ' + str(score[type_token_ratio]))
+
+    # Lexical related
+    # ___lexical_density
+    s = set()
+    s.update(preprocessed)
+    score[lexical_density] = float(len(s)/len(raw))
+    # l.resultHere('lexical_density: ' + str(score[lexical_density]))
+
+    score[awl] = len(set(preprocessed) & set(cc.AWL))
+
+    # Character related
+    # ___char_per_word
+    for char in voca:
+        score[char_per_word] += len(char)
+    score[char_per_word] = float(score[char_per_word]/len(voca))
+    # l.resultHere('char_per_word: ' + str(score[char_per_word]))
+
+    score[document_length] = len(raw)
+
+    # ___syllable_count
+    score[syllable_count] = avgSyllableCount(preprocessed)
+
+    return [score[type_token_ratio],
+            score[correct_type_token_ratio],
+            score[lexical_density],
+            score[char_per_word],
+            score[syllable_count],
+            score[document_length],
+            score[awl]]
+
+
+def avgSyllableCount(words):
+    syllable = []
+    for w in words:
+        syllable.append(h.syllableCount(w))
+    return float(sum(syllable))/float(len(syllable))
 
 
 def analizeResult(result):
